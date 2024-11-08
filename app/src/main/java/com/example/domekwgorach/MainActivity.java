c3f6a9d0e4ae37a4d2acbc5dd3f44f474c1909d3
9578845u <posilabartosz@gmail.com>
2024-11-08 12:20:18 +0100

    DomWGorach

--- app/src/main/java/com/example/domekwgorach/MainActivity.java
+++ app/src/main/java/com/example/domekwgorach/MainActivity.java
@@ -1,51 +1,45 @@
 package com.example.domekwgorach;
 
-import android.annotation.SuppressLint;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.lifecycle.ViewModelProvider;
+
 
 public class MainActivity extends AppCompatActivity {
-    private Button button1, button2, button3;
-    private TextView Counter;
-    private int LikeCount =0;
+   private TextView TextViewLike;
+   private Button like;
+   private Button usun;
+   private int likes=0;
 
+   @Override
+    protected void onCreate(Bundle savedInstanceState){
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
 
+       TextViewLike = findViewById(R.id.textView5);
+       like = findViewById(R.id.like);
+       usun = findViewById(R.id.usun);
 
-
-
-    @SuppressLint("MissingInflatedId")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    Button button1=findViewById(R.id.button1);
-    Button button2=findViewById(R.id.button2);
-    Button button3=findViewById(R.id.button3);
-     Counter=findViewById(R.id.textView5);
-
-
-    button2.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            LikeCount++;
-
+       like.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               likes++;
+               TextViewLike.setText(likes + " polubień");
+           }
+       });
+       usun.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               if(likes>0){
+                   likes--;
+                   TextViewLike.setText(likes + " polubień");
+               }
+           }
+       });
+   }
         }
-    });
-    button1.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-
-        }
-    });
-
-        }
-    }
