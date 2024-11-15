0306dbd94711bccd53673ec3cdafb356704ecdfa
9578845u <posilabartosz@gmail.com>
2024-11-15 12:10:44 +0100

    DomWGorach

--- app/src/main/java/com/example/domekwgorach/MainActivity.java
+++ app/src/main/java/com/example/domekwgorach/MainActivity.java
@@ -1,26 +1,21 @@
 package com.example.domekwgorach;
 
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-
 import androidx.appcompat.app.AppCompatActivity;
-
 
 public class MainActivity extends AppCompatActivity {
    private TextView TextViewLike;
    private Button like;
    private Button usun;
-   private int likes=0;
+   private int polubienia=0;
 
    @Override
     protected void onCreate(Bundle savedInstanceState){
        super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_main);
-
        TextViewLike = findViewById(R.id.textView5);
        like = findViewById(R.id.like);
        usun = findViewById(R.id.usun);
@@ -28,18 +23,18 @@ public class MainActivity extends AppCompatActivity {
        like.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
-               likes++;
-               TextViewLike.setText(likes + " polubień");
+               polubienia++;
+               TextViewLike.setText(polubienia + " polubień");
            }
        });
        usun.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
-               if(likes>0){
-                   likes--;
-                   TextViewLike.setText(likes + " polubień");
+               if(polubienia>0){
+                   polubienia--;
+                   TextViewLike.setText(polubienia + " polubień");
                }
            }
        });
    }
-        }
+}
